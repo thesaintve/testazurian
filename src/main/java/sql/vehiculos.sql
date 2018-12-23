955eb680c60c8f3e1db84c10fc627409808b193b
David Besson <david.besson@gmail.com>
2018-12-23 15:02:21 -0500

    Cambios en SQL

--- src/main/java/sql/vehiculos.sql
+++ src/main/java/sql/vehiculos.sql
@@ -4,6 +4,10 @@
 -- 4) Abrir el proyecto en esta ruta http://localhost:8080/mavenSpringRest/faces/index.xhtml
 -- 5) El endpint Rest http://localhost:8080/mavenSpringRest/vehiculo
 
+--  Sitio donde esta en produccion
+--  http://35.175.88.180:8585/mavenSpringRest-1.0/faces/index.xhtml
+--  El endpint Rest http://35.175.88.180:8585/mavenSpringRest-1.0/vehiculo
+
 CREATE TABLE vehiculo
 (
   matricula VARCHAR(6) NOT NULL,
